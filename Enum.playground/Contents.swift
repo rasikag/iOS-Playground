561576ba2d54228b7cf862e9ac057b0fd36c1b14
rasikag <rasikagayan90@gmail.com>
2018-03-01 23:15:45 +0800

    enum syntax

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -1,3 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
+// syntax
+enum NameOfEnum {
+    // like C# enum
+    case Hello01
+    case Hello02
+    case Hello03
+    //or
+    case Hello04, Hello05
+}
+
+// how to use 
+let giveMeValue : NameOfEnum = .Hello04
+
+
