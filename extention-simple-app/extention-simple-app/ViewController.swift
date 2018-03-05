13e42670199212e794713c515b5a939668d10d48
rasikag <rasikagayan90@gmail.com>
2018-03-05 23:22:48 +0800

    simple extension app

---
+++ extention-simple-app/extention-simple-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  extention-simple-app
+//
+//  Created by Rasika Gayan  Gunarathna  on 5/3/18.
+//  Copyright © 2018 idea-capsule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
