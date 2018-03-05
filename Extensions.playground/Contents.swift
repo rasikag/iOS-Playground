3b215239f694b34e49ea09dc59a62dee226e32a4
rasikag <rasikagayan90@gmail.com>
2018-03-05 22:45:57 +0800

    extenstions with mutating

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -9,3 +9,31 @@ class TestExtension001{
 extension TestExtension001{
     
 }
+
+
+extension String{
+    
+    func reverse() -> String {
+        var characterArray = [Character]()
+        for character in self.characters{
+            characterArray.insert(character, at: 0)
+        }
+        return String(characterArray)
+    }
+}
+
+var name = "My name is Khan"
+
+name.reverse()
+
+
+extension Double{
+    mutating func calculateArea() -> Double{
+        self = 3.1414 * self * self;
+        return self
+    }
+}
+
+var radius = 3.345
+radius.calculateArea()
+print(radius)
