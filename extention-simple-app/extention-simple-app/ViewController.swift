f04ed81374f1af213c3dbaae98949b14576e66ce
rasikag <rasikagayan90@gmail.com>
2018-03-06 00:07:36 +0800

    buttons added

--- extention-simple-app/extention-simple-app/ViewController.swift
+++ extention-simple-app/extention-simple-app/ViewController.swift
@@ -1,25 +1,16 @@
-//
-//  ViewController.swift
-//  extention-simple-app
-//
-//  Created by Rasika Gayan  Gunarathna  on 5/3/18.
-//  Copyright © 2018 idea-capsule. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wiggleBtn: UIButton!
+    @IBOutlet weak var colorizeBtn: UIButton!
+    @IBOutlet weak var dimBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
