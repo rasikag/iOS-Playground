357ce72c6e340f9586d22558b8fdb02113e6ddad
rasikag <rasikagayan90@gmail.com>
2018-03-16 17:13:27 +0800

    protocol done

--- protocol_01.playground/Contents.swift
+++ protocol_01.playground/Contents.swift
@@ -31,3 +31,75 @@ func /(valueA: Number, valueB: Number) -> Float{
     return valueA.floatValue / valueB.floatValue
 }
 
+
+class Question{
+    var type: QuestionType
+    var query: String
+    var answer: String
+    
+    init(type: QuestionType, query: String, answer: String) {
+        self.type = type
+        self.query = query
+        self.answer = answer
+    }
+}
+
+enum QuestionType: String {
+    
+    case trueFalse = "The sky is blue"
+    case multipleChoice = "Who is the ugliest beatle? John, Paul, Geroger or Ringo"
+    case shortAnswer = "What is the capital of Sri Lanka"
+    case essay = "Tell me about your self"
+    
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay]
+    
+}
+
+enum AnswerType: String {
+    
+    case trueFalse = "The sky is blue"
+    case multipleChoice = "Who is the ugliest beatle? John, Paul, Geroger or Ringo"
+    case shortAnswer = "What is the capital of Sri Lanka"
+    case essay = "Tell me about your self"
+    
+    static let types = [trueFalse, multipleChoice, shortAnswer, essay]
+    
+}
+
+
+protocol QuestionGenerator{
+    
+    func generateRandomQuestion() -> Question
+    
+}
+
+class Quiz: QuestionGenerator {
+    
+    func generateRandomQuestion() -> Question {
+        let randomNumeral = Int(arc4random_uniform(4))
+        let randomType = QuestionType.types[randomNumeral]
+        let randomQuery = randomType.rawValue
+        let randomAnswer = AnswerType.types[randomNumeral].rawValue
+        let randomQuestion = Question(type: randomType, query: randomQuery, answer: randomAnswer)
+        return randomQuestion
+    }
+}
+
+let quiz = Quiz()
+
+let question = quiz.generateRandomQuestion()
+
+print("Question type: \(question.type) \nQuery: \(question.query) \nAnswer: \(question.answer)")
+
+
+
+
+
+
+
+
+
+
+
+
+
