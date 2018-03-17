b0cd31447ff2f60f4311b94d753ded3c2d93c08b
rasikag <rasikagayan90@gmail.com>
2018-03-17 18:14:16 +0800

    programmatic segue

--- app-basketball/app-basketball/Controller/LeagueVC.swift
+++ app-basketball/app-basketball/Controller/LeagueVC.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier:"skillVCSegue", sender: self)
+    }
+    
+    
 }
