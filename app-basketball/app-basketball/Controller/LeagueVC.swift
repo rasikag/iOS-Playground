883bb39b10009468cab8a6b645742a48826b5859
rasikag <rasikagayan90@gmail.com>
2018-03-17 19:52:15 +0800

    button action

--- app-basketball/app-basketball/Controller/LeagueVC.swift
+++ app-basketball/app-basketball/Controller/LeagueVC.swift
@@ -10,13 +10,36 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BoarderButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextBtn.isEnabled = false
+        player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier:"skillVCSegue", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
     
 }
---
+++ app-basketball/app-basketball/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-basketball
+//
+//  Created by Rasika Gayan  Gunarathna  on 17/3/18.
+//  Copyright © 2018 idea-capsule. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
