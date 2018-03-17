a1ac7a8d3034507f0c94980718e6729b741b23ff
rasikag <rasikagayan90@gmail.com>
2018-03-17 10:29:39 +0800

    before adding auto layout

--- app-basketball/app-basketball/Views/BoarderButton.swift
+++ app-basketball/app-basketball/Views/BoarderButton.swift
@@ -2,7 +2,7 @@
 //  BoarderButton.swift
 //  app-basketball
 //
-//  Created by Rasika Gayan  Gunarathna  on 16/3/18.
+//  Created by Rasika Gayan Gunarathna  on 16/3/18.
 //  Copyright © 2018 idea-capsule. All rights reserved.
 //
 
@@ -10,12 +10,11 @@ import UIKit
 
 class BoarderButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor =
+            UIColor.white.cgColor
     }
-    */
 
 }
