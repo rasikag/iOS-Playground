c9961bb99240dc587d0105d9db59012615f71972
rasikag <rasikagayan90@gmail.com>
2018-03-19 02:37:22 +0800

    added custom text class

--- shop-now/shop-now/Controller/MainVC.swift
+++ shop-now/shop-now/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  shop-now
 //
 //  Created by Rasika Gayan  Gunarathna  on 19/3/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ shop-now/shop-now/View/CurrencyTextField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTextField.swift
+//  shop-now
+//
+//  Created by Rasika Gayan  Gunarathna  on 19/3/18.
+//  Copyright © 2018 idea-capsule. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        let place = NSAttributedString(string: placeholder!,
+                                       attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+}
