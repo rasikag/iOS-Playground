5a949a171914ac78e1e9cc8966616e6a2a292a30
rasikag <rasikagayan90@gmail.com>
2018-03-20 22:18:17 +0800

    custom button

--- shop-now/shop-now/View/CurrencyTextField.swift
+++ shop-now/shop-now/View/CurrencyTextField.swift
@@ -20,6 +20,14 @@ class CurrencyTextField: UITextField {
     */
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -32,9 +40,5 @@ class CurrencyTextField: UITextField {
                                        attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
         attributedPlaceholder = place
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    }
-    
-    override func prepareForInterfaceBuilder() {
-        
     }
 }
