a8b8d390a071b5d7bbbbc4415936aa6f64d8fec8
rasikag <rasikagayan90@gmail.com>
2018-03-22 00:47:26 +0800

    calculater app done

--- shop-now/shop-now/View/CurrencyTextField.swift
+++ shop-now/shop-now/View/CurrencyTextField.swift
@@ -31,7 +31,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 0.9961728454, green: 0.9902502894, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
+        clipsToBounds = true
         if placeholder == nil {
             placeholder = " "
         }
@@ -40,5 +40,23 @@ class CurrencyTextField: UITextField {
                                        attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
         attributedPlaceholder = place
         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5,
+                                                y: (frame.size.height / 2) - size / 2 ,
+                                                width: size,
+                                                height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.370555222, green: 0.3705646992, blue: 0.3705595732, alpha: 1)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
     }
 }
