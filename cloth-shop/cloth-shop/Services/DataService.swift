e3f1a994e460cc44b9c687d3f0446fb3ab6667a7
rasikag <rasikagayan90@gmail.com>
2018-04-10 21:48:07 +0800

    table view

---
+++ cloth-shop/cloth-shop/Services/DataService.swift
@@ -0,0 +1,25 @@
+//
+//  DataService.swift
+//  cloth-shop
+//
+//  Created by Rasika Gayan  Gunarathna  on 10/4/18.
+//  Copyright © 2018 idea-capsule. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
