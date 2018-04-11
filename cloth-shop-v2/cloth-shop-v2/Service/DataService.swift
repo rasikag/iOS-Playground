9db35e4d52348d3d7a5e55ea946eae29810d50a4
rasikag <rasikagayan90@gmail.com>
2018-04-12 02:19:28 +0800

    fix the scroll bar

--- cloth-shop-v2/cloth-shop-v2/Service/DataService.swift
+++ cloth-shop-v2/cloth-shop-v2/Service/DataService.swift
@@ -15,8 +15,8 @@ class DataService {
     private let categories = [
         Category(title: "SHIRTS", imageName: "shirts.png"),
         Category(title: "HOODIES", imageName: "hoodies.png"),
-        Category(title: "HATS", imageName: "hats.png")
-        
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
     ]
     
     func getCategories() -> [Category] {
