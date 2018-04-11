8049412ec4e5a4338dc119bda53d00f9d37fb8a0
rasikag <rasikagayan90@gmail.com>
2018-04-11 23:50:29 +0800

    cloth shop V2

---
+++ cloth-shop-v2/cloth-shop-v2/CategoriesVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cloth-shop-v2
+//
+//  Created by Rasika Gayan  Gunarathna  on 11/4/18.
+//  Copyright © 2018 idea-capsule. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
