abd7ba80f9d6501c0aaff4bfba55bfbc0f78a42c
rasikag <rasikagayan90@gmail.com>
2018-04-13 00:06:17 +0800

    collection view 02

---
+++ cloth-shop-v2/cloth-shop-v2/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  cloth-shop-v2
+//
+//  Created by Rasika Gayan  Gunarathna  on 12/4/18.
+//  Copyright © 2018 idea-capsule. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var procutPrice: UILabel!
+    
+}
