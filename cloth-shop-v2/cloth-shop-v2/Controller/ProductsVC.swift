7a7306b176dad7c7124f5fd7e6b1735e5a6972df
rasikag <rasikagayan90@gmail.com>
2018-04-15 19:25:54 +0800

    set segue

--- cloth-shop-v2/cloth-shop-v2/Controller/ProductsVC.swift
+++ cloth-shop-v2/cloth-shop-v2/Controller/ProductsVC.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ProductsVC: UIViewController {
+    
+    private(set) public var products = [Product]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
     }
     
 }
